78c0dc2f9b09fc31aefb12dcb815474fa9540727
Ionut Balica <balicaionut@gmail.com>
2024-02-25 15:16:03 +0200

    Refactor SixFortyNine.vue component to add functionality for checking code accuracy

--- src/components/SixFortyNine.vue
+++ src/components/SixFortyNine.vue
@@ -1,26 +1,114 @@
 <template>
-    <div>
-        <div id="numbers">
-          <h2>Generated Numbers:</h2>
-            <div id="cell-list">
-                <button
-                  v-for="(number, index) in numbersList"
-                  :key="index" style="width:40px;height:40px;border:1px solid #000;"
-                >
-                    {{ number }}
-                </button>
-            </div>
-        </div>
-        <div style="padding: 20px">
-            <button @click="convertExcelToJson">Generate</button>
-        </div>
+  <div>
+    <div style="display:flex; flex-direction: row; justify-content: center;">
+      <div style="padding: 10px 0 10px 0">
+        Generate numbers
+      </div>
+      <div style="padding: 10px">
+        <label for="switchCheckCombination" class="switch">
+          <input
+            id="switchCheckCombination"
+            type="checkbox"
+            @click="isCheckAccuracy = !isCheckAccuracy">
+          <span class="slider round"></span>
+        </label>
+      </div>
+      <div style="padding: 10px 0 10px 0">
+        Check code accuracy
+      </div>
     </div>
+    <div v-if="!isCheckAccuracy">
+      <div id="numbers">
+      <h2>Generated Numbers:</h2>
+      <div id="cell-list">
+        <button
+          v-for="(number, index) in numbersList"
+          :key="index"
+          style="width:40px;height:40px;border:1px solid #000;"
+        >
+          {{ number }}
+        </button>
+      </div>
+    </div>
+    <div style="padding: 20px">
+      <button @click="convertExcelToJson">Generate</button>
+    </div>
+    </div>
+    <div v-if="isCheckAccuracy">
+      <h2>Insert latest combination:</h2>
+      <div>
+        <label for="latestCombination">
+          <input
+            type="text"
+            id="latestCombination"
+            v-model="latestCombination"
+            @input="checkCombinationFormat"
+          />
+        </label>
+        <div v-if="inputError" style="color: red;">{{ inputError }}</div>
+      </div>
+      <div>example: &lt;21 32 13 4 45 26></div>
+      <div style="padding: 20px">
+        <button
+          @click="checkCodeAccuracy"
+          :disabled="!!inputError || !latestCombination"
+        >
+          Check accuracy
+        </button>
+      </div>
+      <div v-for="item in matchLog" :key="item">
+        {{ item }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 // eslint-disable-next-line import/no-extraneous-dependencies
 import * as XLSX from 'xlsx';
 import { ref } from 'vue';
+
+const isCheckAccuracy = ref(false);
+
+const latestCombination = ref('');
+const inputError = ref('');
+const latestNumbers = ref([]);
+const countMatches = ref(0);
+const countGenerated = ref(0);
+const matchLog = ref([]);
+
+const checkCombinationFormat = (event) => {
+  console.log('combination', event.target.value);
+
+  const input = event.target.value.trim();
+  const numbers = input.split(' ');
+
+  if (numbers.length !== 6) {
+    inputError.value = 'Please enter 6 numbers separated by spaces';
+    return;
+  }
+
+  if (!numbers.every((number) => /^\d+$/.test(number))) {
+    inputError.value = 'Please enter only numeric values';
+    return;
+  }
+
+  inputError.value = '';
+};
+
+const checkCodeAccuracy = async () => {
+  matchLog.value = [];
+  latestNumbers.value = latestCombination.value.split(' ').map((num) => parseInt(num, 10));
+  // eslint-disable-next-line no-use-before-define
+  if (!jsonData.value.length) {
+    // eslint-disable-next-line no-use-before-define
+    await convertExcelToJson();
+  } else {
+    countGenerated.value = 0;
+    // eslint-disable-next-line no-use-before-define
+    generateNumbers();
+  }
+};
 
 const numbersList = ref([]);
 
@@ -92,15 +180,100 @@ const generateNumbers = async () => {
   }
 
   numbersList.value = newNumbers;
+
+  if (isCheckAccuracy.value) {
+    latestNumbers.value.forEach((number) => {
+      if (newNumbers.includes(number)) {
+        countMatches.value += 1;
+      }
+    });
+    countGenerated.value += 1;
+    if (countGenerated.value < 5000) {
+      if (countMatches.value === 4) {
+        matchLog.value.push(`4 matches at ${countGenerated.value}`);
+        console.log(matchLog.value);
+      }
+      if (countMatches.value === 5) {
+        matchLog.value.push(`5 matches at ${countGenerated.value}`);
+        console.log(matchLog.value);
+      }
+      if (countMatches.value === 6) {
+        matchLog.value.push(`6 matches at ${countGenerated.value}`);
+        console.log(matchLog.value);
+      }
+      countMatches.value = 0;
+      generateNumbers();
+    }
+  }
 };
 </script>
 
 <style>
 #numbers {
-    padding-top: 20px;
+    padding-top: 10px;
 }
 
 #cell-list button {
     display: inline;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 30px;
+  height: 17px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 13px;
+  width: 13px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(13px);
+  -ms-transform: translateX(13px);
+  transform: translateX(13px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 17px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
